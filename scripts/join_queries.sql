7a5f042ece9deaf533c04704f28d558323395fba
Lukas Augustynowicz <lukasaugustynowiczz@gmail.com>
2025-03-16 12:52:18 -0400

    fix workload

--- scripts/join_queries.sql
+++ scripts/join_queries.sql
@@ -1,54 +1,44 @@
-
+    ---------------vary inner
     EXPLAIN ANALYZE SELECT COUNT(*)
     FROM Table1 AS R, Table2 AS S
-    WHERE R.ID = S.ID AND
-    R.ID < 10000 AND
-    S.ID > 0;
+    WHERE R.a = S.a AND
+    R.a < 1000 AND
+    S.a > 500;
     
     EXPLAIN ANALYZE SELECT COUNT(*)
     FROM Table1 AS R, Table2 AS S
-    WHERE R.ID = S.ID AND
-    R.ID < 10000 AND
-    S.ID > 3333;
+    WHERE R.a = S.a AND
+    R.a < 5000 AND
+    S.a > 500;
 
     EXPLAIN ANALYZE SELECT COUNT(*)
     FROM Table1 AS R, Table2 AS S
-    WHERE R.ID = S.ID AND
-    R.ID < 10000 AND
-    S.ID > 6666;
+    WHERE R.a = S.a AND
+    R.a < 8000 AND
+    S.a > 500;
+-------------------------- vary bf size
+     EXPLAIN ANALYZE SELECT COUNT(*)
+    FROM Table1 AS R, Table2 AS S
+    WHERE R.a = S.a AND
+    R.a < 3000 AND
+    S.a > 500;
+    
+------------------------ vary outer
+
+     EXPLAIN ANALYZE SELECT COUNT(*)
+    FROM Table1 AS R, Table2 AS S
+    WHERE R.a = S.a AND
+    R.a < 2000 AND
+    S.a > 500;
+    
+    EXPLAIN ANALYZE SELECT COUNT(*)
+    FROM Table1 AS R, Table2 AS S
+    WHERE R.a = S.a AND
+    R.a < 2000 AND
+    S.a > 3000;
 
     EXPLAIN ANALYZE SELECT COUNT(*)
     FROM Table1 AS R, Table2 AS S
-    WHERE R.ID = S.ID AND
-    R.ID < 6666 AND
-    S.ID > 0;
-
-    EXPLAIN ANALYZE SELECT COUNT(*)
-    FROM Table1 AS R, Table2 AS S
-    WHERE R.ID = S.ID AND
-    R.ID < 6666 AND
-    S.ID > 3333;
-
-    EXPLAIN ANALYZE SELECT COUNT(*)
-    FROM Table1 AS R, Table2 AS S
-    WHERE R.ID = S.ID AND
-    R.ID < 6666 AND
-    S.ID > 6666;
-
-    EXPLAIN ANALYZE SELECT COUNT(*)
-    FROM Table1 AS R, Table2 AS S
-    WHERE R.ID = S.ID AND
-    R.ID < 3333 AND
-    S.ID > 0;
-
-    EXPLAIN ANALYZE SELECT COUNT(*)
-    FROM Table1 AS R, Table2 AS S
-    WHERE R.ID = S.ID AND
-    R.ID < 3333 AND
-    S.ID > 3333;
-
-    EXPLAIN ANALYZE SELECT COUNT(*)
-    FROM Table1 AS R, Table2 AS S
-    WHERE R.ID = S.ID AND
-    R.ID < 3333 AND
-    S.ID > 6666;
+    WHERE R.a = S.a AND
+    R.a < 2000 AND
+    S.a > 7000;
